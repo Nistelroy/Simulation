2f76d2ccaf5afad3a37b38d097d99ba5df28d73f
Nistel <duz3000@gmail.com>
2023-10-05 21:16:02 +0300

    add render one step

--- src/main/java/ru/yandex/practicum/Simulation.java
+++ src/main/java/ru/yandex/practicum/Simulation.java
@@ -1,27 +1,40 @@
 package ru.yandex.practicum;
-import ru.yandex.practicum.process.*;
+
+import ru.yandex.practicum.process.Action;
+import ru.yandex.practicum.process.Map;
+import ru.yandex.practicum.process.MapRender;
+
+import java.util.Scanner;
 
 public class Simulation {
    public static final int XX = 10;
    public static final int YY = 10;
     public static void main(String[] args) throws InterruptedException {
-
+        Scanner scanner = new Scanner(System.in);
         Map map = new Map();
         MapRender mapRender = new MapRender();
         mapRender.creatingTheNewWorld(map);
         Action action = new Action();
 
-        while (true) {
-            System.out.println();
-            mapRender.render(map);
-            Thread.sleep(1000);
-            action.nextTurn(map);
+       while (true) {
+           System.out.println();
+           System.out.println("Cделать один ход введите ' 1 ', запустить симуляцию введите ' 2 ' ");
+           int userInput = scanner.nextInt();
+           if (userInput == 2) {
+               while (true) {
+                   System.out.println();
+                   mapRender.render(map);
+                   Thread.sleep(1000);
+                   action.nextTurn(map);
+               }
+           } else if (userInput == 1) {
+               System.out.println();
+               mapRender.render(map);
+               action.nextTurn(map);
+           } else System.out.println("Неверный ввод. Cделать один ход введите ' 1 ', запустить симуляцию введите ' 2 ' ");
 
 
        }
-
-
-
     }
 
 
